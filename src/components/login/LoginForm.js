0a9455a5624aa9949200354537309629b6d34ddf
Luiz Jurazek <luizjurazek@gmail.com>
2024-09-12 21:06:23 -0300

    feat: add functions to capture data of forms

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,8 +1,21 @@
-import React from "react";
-import Style from "../../css/LoginForm.module.css"
-import logoGoogle from "../../assets/img/logoGoogle.svg"
+import React, { useState } from "react";
+import Style from "../../css/LoginForm.module.css";
+import logoGoogle from "../../assets/img/logoGoogle.svg";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function setInputs(e, setInput) {
+    setInput(e.target.value);
+  }
+
+  function submitLoginButton(){
+    // criar a regra de login aqui
+    console.log(email)
+    console.log(password)
+  }
+
   return (
     <>
       <section className={Style.container}>
@@ -14,16 +27,33 @@ export default function LoginForm() {
         {/* Form */}
         <section className={Style.FormSection}>
           <label for="email">Email</label>
-          <input type="email" placeholder="Enter your email" id="email" />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            id="email"
+            onChange={(e) => {
+              setInputs(e, setEmail);
+            }}
+          />
           <label for="password">Password</label>
-          <input type="password" placeholder="Password" id="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={(e) => {
+              setInputs(e, setPassword);
+            }}
+          />
           <a href="/">Forgot password</a>
         </section>
 
         {/* Botoes de login e sign */}
         <div className={Style.btnSection}>
-          <button className={Style.btnLogin}>Login</button>
-          <button className={Style.btnLoginGoogle}><img src={logoGoogle} alt="Logo do google" width={20}/>Login with Google</button>
+          <button className={Style.btnLogin} onClick={submitLoginButton}>Login</button>
+          <button className={Style.btnLoginGoogle}>
+            <img src={logoGoogle} alt="Logo do google" width={20} />
+            Login with Google
+          </button>
           <p>
             Don't have an account? <a href="/">Sign up for free</a>
           </p>
--- src/components/signIn/SignInForm.js
+++ src/components/signIn/SignInForm.js
@@ -1,8 +1,26 @@
-import React from "react";
-import Style from "../../css/SignInForm.module.css"
-import logoGoogle from "../../assets/img/logoGoogle.svg"
+import React, { useState } from "react";
+import Style from "../../css/SignInForm.module.css";
+import logoGoogle from "../../assets/img/logoGoogle.svg";
 
 export default function SignIn() {
+  const [name, setName] = useState();
+  const [birthday, setBirthday] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setconfirmPassword] = useState();
+
+  function setInputs(e, setInput) {
+    setInput(e.target.value);
+  }
+
+  function signSubmitedButton() {
+    console.log(name);
+    console.log(birthday);
+    console.log(email);
+    console.log(password);
+    console.log(confirmPassword);
+  }
+
   return (
     <>
       <section className={Style.container}>
@@ -14,19 +32,26 @@ export default function SignIn() {
         {/* Form */}
         <section className={Style.FormSection}>
           <label for="name">Name</label>
-          <input type="text" placeholder="Enter your name" id="name" />
+          <input type="text" placeholder="Enter your name" id="name" onChange={(e) => setInputs(e, setName)} />
           <label for="name">Birthday</label>
-          <input type="date" placeholder="Enter your birthday" id="birthday" />
+          <input type="date" placeholder="Enter your birthday" id="birthday" onChange={(e) => setInputs(e, setBirthday)} />
           <label for="email">Email</label>
-          <input type="email" placeholder="Enter your email" id="email" />
+          <input type="email" placeholder="Enter your email" id="email" onChange={(e) => setInputs(e, setEmail)} />
           <label for="password">Password</label>
-          <input type="password" placeholder="Password" id="password" />
+          <input type="password" placeholder="Password" id="password" onChange={(e) => setInputs(e, setPassword)} />
+          <label for="confirmPassword">Repeat your password</label>
+          <input type="password" placeholder="Confirm your password" id="confirmPassword" onChange={(e) => setInputs(e, setconfirmPassword)} />
         </section>
 
         {/* Botoes de login e sign */}
         <div className={Style.btnSection}>
-          <button className={Style.btnSignIn}>Sign In</button>
-          <button className={Style.btnSignInGoogle}><img src={logoGoogle} alt="Logo do google" width={20}/>Sign in With Google</button>
+          <button className={Style.btnSignIn} onClick={signSubmitedButton}>
+            Sign In
+          </button>
+          <button className={Style.btnSignInGoogle}>
+            <img src={logoGoogle} alt="Logo do google" width={20} />
+            Sign in With Google
+          </button>
           <p>
             Already have an account? <a href="/">Login</a>
           </p>
